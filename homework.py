4705831ac9c14d8df2f30efa69c627ab9ce506f0
white <white@debian.monstrix.local>
2021-01-29 22:08:50 +0300

    check flake8

--- homework.py
+++ homework.py
@@ -1,8 +1,34 @@
 import datetime as dt
 
 
+class Calculator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+
+    def add_record(self, record):
+        self.records.append(record)
+
+    def get_today_stats(self):
+        today = dt.date.today()
+        return sum(record.amount
+                   for record in self.records
+                   if record.date == today)
+
+    def get_week_stats(self):
+        today = dt.date.today()
+        week = dt.timedelta(days=6)
+        return sum(record.amount
+                   for record in self.records
+                   if today - week <= record.date <= today)
+
+    def today_balance(self):
+        day_balance = self.limit - self.get_today_stats()
+        return day_balance
+
+
 class Record:
-    def __init__(self, amount, comment, date = None):
+    def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
         if date is None:
@@ -10,76 +36,37 @@ class Record:
         else:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
 
-
     def show(self):
-        """formatted data output"""
         return self.amount, self.comment, self.date
 
 
-class Calculator:
-    def __init__(self, limit):
-        self.limit = limit
-        self.records = []
-
-
-    def add_record(self, record):
-        """add new record"""
-        self.records.append(record)
-
-
-    def get_today_stats(self):
-        """count records for today"""
-        today = dt.date.today()
-        return sum(record.amount
-                for record in self.records
-                if record.date == today)
-
-
-    def get_week_stats(self):
-        """count records for one week"""
-        today = dt.date.today()
-        week = dt.timedelta(days = 6)
-        return sum(record.amount
-                for record in self.records
-                if today - week <= record.date <= today)
-
-
-    def today_count(self):
-        """limit calculation"""
-        day_counter = self.limit - self.get_today_stats()
-        return day_counter
-
-
-class CaloriesCalculator(Calculator):
-    def get_calories_remained(self):
-        """counting calories"""
-        today_count = self.today_count()
-        if today_count > 0:
-            return ('Сегодня можно съесть что-нибудь ещё,'
-                    f'но с общей калорийностью не более {today_count} кКал')
-        return f'Хватит есть!'
-
-
 class CashCalculator(Calculator):
-    EURO_RATE = 91.85
-    USD_RATE = 75.69
-
+    EURO_RATE = 90.5
+    USD_RATE = 70.5
 
     def get_today_cash_remained(self, currency):
-        """counting money in different currencies"""
         course = {
             'rub': (1, 'руб'),
             'usd': (CashCalculator.USD_RATE, 'USD'),
             'eur': (CashCalculator.EURO_RATE, 'Euro')
-     }
+        }
         if currency not in course:
             return 'Неподдерживаемая валюта'
-        today_count = self.today_count()
-        if today_count == 0:
-            return f'Денег нет, держись'
-        rate, valuta = course[currency]
-        balance = round(today_count / rate, 2)
+        today_balance = self.today_balance()
+        if today_balance == 0:
+            return 'Денег нет, держись'
+        rate, currency_name = course[currency]
+        balance = round(today_balance / rate, 2)
         if balance > 0:
-            return f'На сегодня осталось {balance} {valuta}'
+            return f'На сегодня осталось {balance} {currency_name}'
         balance = abs(balance)
-        return f'Денег нет, держись: твой долг - {balance} {valuta}'
+        return f'Денег нет, держись: твой долг - {balance} {currency_name}'
+
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        today_balance = self.today_balance()
+        if today_balance > 0:
+            return ('Сегодня можно съесть что-нибудь ещё, но с общей '
+                    f'калорийностью не более {today_balance} кКал')
+        return 'Хватит есть!'
